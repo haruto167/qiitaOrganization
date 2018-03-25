d42475a71b91e6c9360c8ee539047db69f428946
root <root@ik1-336-28113.vs.sakura.ne.jp>
2018-03-25 12:14:38 +0900

    sort rank]

--- qiitaGet.py
+++ qiitaGet.py
@@ -19,6 +19,36 @@ class super_QiitaClient(QiitaClient):
 def ranksort(data):
     return sorted(data.items(), key=lambda item: item[1])
 
+def dctSortDesc(dct):
+    for k, v in sorted(dct.items(), key=lambda x: -x[1]):
+      print(str(k) + ": " + str(v))
+
+def countArray(arrays):
+    arrange = {}
+    maxArray = len(arrays)
+    i = 0
+    tmp = ''
+    before = ''
+    j = 1
+    for i in range(0,maxArray):
+        tmp = arrays[i]
+        if(tmp == before):
+            j = j + 1
+        else:
+            arrange[before] = j
+            j = 1
+        before = arrays[i]
+    return arrange
+
+def rankGet(dct,rank):
+    i = 0
+    for key, value in dct.items():
+        i += 1
+        if i < rank:
+          print('{}: {}'.format(key, value))
+        else:
+          return rank
+
 accessToken = '6bc2c28ebed49f98070caab7668da51a16b1df0c'
 client = super_QiitaClient(access_token=accessToken)
 i = 0
@@ -35,4 +65,9 @@ for i in range(1,maxArray):
          ora.append(strOrigin.encode('utf-8'))
 
 ora2 = sorted(ora)
-print(ora2)
+#print(ora2)
+dic = {}
+dic = countArray(ora2)
+dic = dctSortDesc(dic)
+
+#rank = rankGet(dic,10)
